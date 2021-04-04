9e5a325301e1ba0228a6b5220d01cdfbfc754b7c
Haizi Zheng <haizi.zh@gmail.com>
2021-04-04 10:35:45 -0400

    Improve BED IO

--- R/io.R
+++ R/io.R
@@ -8,6 +8,10 @@ is_gzip <- function(file_path) {
             pattern = "\\.") %>% map_chr( ~ tail(., n = 1)) %in% c("gz", "bgz")
 }
 
+is_remote <- function(file_path) {
+  str_detect(file_path, pattern = "^[^:]+://")
+}
+
 
 # Make sure all ranges are valid
 normalize_tabix_range <- function(range) {
@@ -54,14 +58,103 @@ post_process_table <- function(dt) {
 }
 
 
+#' Check if required binaries exist in PATH
+#'
+#' Some of cragr's functionalities require certain third-party binaries to exist
+#' in PATH. For example, selectively loading cfDNA fragments from a certain
+#' region under the hood invokes tabix to complete the task. This function checks
+#' whether certain binaries exist.
+#' @param binaries Character vector indicating what binaries to check. Default
+#'   is tabix.
+#' @param verbose Logical value indicating whether to output detailed messages.
+#'   Default is FALSE.
+#' @param stop_on_fail Stop the script completely if the result is `FALSE`.
+#' @return Logical value. TRUE if all required binaries can be found in PATH.
+#' @export
+check_binaries <- function(binaries = "tabix", verbose = FALSE, stop_on_fail = FALSE) {
+  check_results <- binaries %>% purrr::map_lgl(function(binary) {
+    # check if binary is in path
+    if ("" == Sys.which(binary)) {
+      if (verbose) {
+        message(str_interp("Checking ${binary}: FAIL"))
+      }
+      return(FALSE)
+    }
+    else {
+      if (verbose) {
+        message(str_interp("Checking ${binary}: PASS"))
+      }
+      return(TRUE)
+    }
+  })
+  
+  check_results <- all(check_results)
+  if (stop_on_fail)
+    assertthat::assert_that(check_results, msg = paste0("Failed in locating ", paste(binaries, collapse = ", ")))
+  else
+    check_results
+}
+
+
+filter_by_region <- function(dt, range) {
+  stopifnot(length(range) == 1)
+  
+  if (str_detect(range, pattern = "^[^:]+$"))
+    return(dt[chrom == range])
+  
+  m <- str_match(range, pattern = "([^:]+):([0-9]+)-([0-9]+)")
+  if (is.na(m[1, 1]))
+    stop(str_interp("Invalid range: ${range}"))
+  else {
+    c1 <- m[1, 2]
+    s1 <- as.integer(m[1, 3]) - 1L
+    e1 <- as.integer(m[1, 4])
+    return(dt[chrom == c1 & ((end > s1 & end <= e1) | (start < e1 & start >= s1))])
+  }
+}
+
+
 # Load a gzipped and indexed BED-like file
-read_tabix_bed <- function(file_path, range) {
+read_tabix_bed <- function(file_path, range, use_htslib = FALSE) {
   # Make sure the genomic ranges are valid
   range <- normalize_tabix_range(range)
-  dt <- seqminer::tabix.read.table(file_path, tabixRange = range)
-  data.table::setDT(dt)
+
+  na_strings <- c("NA", "na", "NaN", "nan", ".", "")
+  filter_flag <- FALSE
+  if (!use_htslib) {
+    # Check if we can use tabix to extract the reads
+    tabix_exist <- check_binaries(binaries = "tabix", verbose = FALSE, stop_on_fail = TRUE)
+    index_exist <- file.exists(paste0(file_path, ".tbi"))
+    use_tabix <- tabix_exist && index_exist
+    
+    if (use_tabix) {
+      tabix_regions <- paste0(as.character(range), collapse = " ")
+      tabix_cmd <- str_interp("tabix ${file_path} ${tabix_regions}")
+      dt <-
+        data.table::fread(cmd = tabix_cmd, na.strings = na_strings)
+    } else {
+      if (endsWith(file_path, ".gz")) {
+        warning("Either tabix or index does not exist. Resource to sequential scanning, which may negatively impact the performance.")
+      }
+      dt <- data.table::fread(file = file_path, na.strings = na_strings)
+      # Postpone the filtering to after assigning column names
+      filter_flag <- TRUE
+    }
+  } else {
+    dt <- seqminer::tabix.read.table(file_path, tabixRange = range)
+    data.table::setDT(dt)
+  }
+  
+  if (nrow(dt) == 0)
+    return(dt)
   
   post_process_table(dt)
+  
+  if (filter_flag) {
+    dt <- filter_by_region(dt, range)
+  }
+  
+  dt
 }
 
 
@@ -101,10 +194,21 @@ read_bed <- function(file_path, range = NULL, ...) {
     dt <- fread(file_path, sep = "\t", na.strings = na_strings, ...)
     post_process_table(dt)
   } else {
+    stopifnot(length(range) == 1)
+    
     if (is_gzip(file_path)) {
       read_tabix_bed(file_path, range)
     } else {
-      stop("range filtering is only available for BGZIP files")
+      if (is_remote(file_path))
+        stop("range filtering is not available for remote BGZIP files")
+      else {
+        # Load directly
+        na_strings <- c("NA", "na", "NaN", "nan", ".", "")
+        dt <- fread(file_path, sep = "\t", na.strings = na_strings, ...)
+        post_process_table(dt)
+        
+        filter_by_region(dt, range)
+      }
     }
   }
 }
