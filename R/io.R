46e98cd6cc06766fa6793bd5c6ed1cfa15e0982b
Haizi Zheng <haizi.zh@gmail.com>
2021-04-13 00:15:04 -0400

    Fix bug in reading BED in non-GenomicRanges mode

--- R/io.R
+++ R/io.R
@@ -199,7 +199,9 @@ read_tabix_bed <- function(file_path, range, index_path = NULL, download_index =
     index_path = if (is.null(index_path)) "" else index_path,
     download_index = download_index
   )
-  read_bed(tempbed)
+  # user_gr should be FALSE, since here we need a data.table object, which later
+  # will be converted to GenomicRanges, if instructed
+  read_bed(tempbed, use_gr = FALSE)
 }
 
 
